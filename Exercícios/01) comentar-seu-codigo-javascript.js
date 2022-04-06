4cdc5b7fac9c4c8dec68d7cb489ac2f3daf1feb1
Carlos Eduardo de Lima <102751741+CarlosEduardoDeLima@users.noreply.github.com>
2022-04-06 19:18:39 -0300

    Delete 01) comentar-seu-codigo-javascript.js

--- Exercícios/01) comentar-seu-codigo-javascript.js
+++
@@ -1,5 +0,0 @@
-// Comentário de uma linha!.
-
-/* Comentário de mais de uma linha! Os comentários são uma ótima
-maneira de deixar anotações para você mesmo e para outras pessoas que mais tarde
-precisarão descobrir o que esse código faz. */
